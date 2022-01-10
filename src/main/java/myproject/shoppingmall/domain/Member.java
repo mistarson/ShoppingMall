cefb25a9e3513d72133391d1a4972535fd2e732d
mistarson <yeowuli2@naver.com>
2022-01-11 00:24:37 +0900

    환경설정 및 데이터베이스 연동

---
+++ src/main/java/myproject/shoppingmall/domain/Member.java
@@ -0,0 +1,26 @@
+package myproject.shoppingmall.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String loginId;
+
+    private String password;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+}
