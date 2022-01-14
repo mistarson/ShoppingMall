0cfce6d493039637f27a4976816bae87eb78e5c3
mistarson <yeowuli2@naver.com>
2022-01-15 01:34:43 +0900

    회원가입 페이지 완성

--- src/main/java/myproject/shoppingmall/controller/MemberController.java
+++ src/main/java/myproject/shoppingmall/controller/MemberController.java
@@ -5,7 +5,11 @@ import myproject.shoppingmall.domain.form.JoinForm;
 import myproject.shoppingmall.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,7 +19,19 @@ public class MemberController {
 
     @GetMapping("/members/new")
     public String createForm(Model model) {
-        model.addAttribute("JoinForm", new JoinForm());
+        model.addAttribute("joinForm", new JoinForm());
         return "members/createJoinForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid JoinForm joinForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "members/createJoinForm";
+        }
+
+        memberService.join(joinForm);
+
+        return "redirect:/";
     }
 }
--- src/main/java/myproject/shoppingmall/domain/form/JoinForm.java
+++ src/main/java/myproject/shoppingmall/domain/form/JoinForm.java
@@ -7,14 +7,14 @@ import javax.validation.constraints.NotEmpty;
 @Data
 public class JoinForm {
 
-    @NotEmpty(message = "이름은 필수입니다.")
-    private String name;
-
     @NotEmpty(message = "아이디는 필수입니다.")
     private String loginId;
 
     @NotEmpty(message = "패스워드는 필수입니다.")
     private String password;
+
+    @NotEmpty(message = "이름은 필수입니다.")
+    private String name;
 
     private String city;
     private String street;
