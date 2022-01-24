08a81d05258ea2d602d1575b5c033ab57b4b5ea5
mistarson <yeowuli2@naver.com>
2022-01-25 01:48:54 +0900

    카테고리 제작 중

--- src/main/java/myproject/shoppingmall/domain/Category.java
+++ src/main/java/myproject/shoppingmall/domain/Category.java
@@ -1,6 +1,7 @@
 package myproject.shoppingmall.domain;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Getter
 @NoArgsConstructor
 public class Category {
 
--- src/main/java/myproject/shoppingmall/dto/CategoryDto.java
+++ src/main/java/myproject/shoppingmall/dto/CategoryDto.java
@@ -1,5 +1,28 @@
 package myproject.shoppingmall.dto;
 
 
+import myproject.shoppingmall.domain.Category;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CategoryDto {
+
+    private String name;
+
+    private String parentCategoryName;
+    private List<CategoryDto> children;
+
+    public CategoryDto(Category category) {
+        this.name = category.getName();
+        this.parentCategoryName = category.getParentCategory() == null ? "clothes" : category.getParentCategory().getName();
+        this.children = category.getChildCategory() == null ?
+                null : category.getChildCategory().stream().map(CategoryDto::new).collect(Collectors.toList());
+    }
+
+    public Category toEntity() {
+        return Category.builder()
+                .name(name)
+                .build();
+    }
 }
