2c5a19f32aefcc6a88ebb5b5cbc88b058744d9ac
mistarson <yeowuli2@naver.com>
2022-06-08 18:27:47 +0900

    Feat: 에러 코드 추가

--- src/main/java/myproject/shoppingmall/global/error/exception/ErrorCode.java
+++ src/main/java/myproject/shoppingmall/global/error/exception/ErrorCode.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 public enum ErrorCode {
 
     // 회원
-    ALREADY_REGISTERED_MEMBER(400, "이미 가입된 회원 입니다.");
+    ALREADY_REGISTERED_MEMBER(400, "이미 가입된 회원 입니다."),
+    NON_EXISTENT_MEMBER(400, "존재하지 않는 회원입니다."),
+
+    // 상품
+    REST_STOCK_NOT_EXISTS(400, "상품의 재고가 부족 합니다."),
+    ALREADY_REGISTERED_ITEM(400, "이미 존재하는 아이템 입니다."),
+    FIRST_ITEM_IMAGE_NOT_EXISTS(400, "첫번째 상품 이미지는 필수 입력 값 입니다.");
 
     ErrorCode(int status, String message) {
         this.status = status;
