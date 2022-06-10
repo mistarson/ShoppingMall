cffeb3ecf0b695be252571943f7983dbf9f03f8b
mistarson <yeowuli2@naver.com>
2022-06-11 00:20:35 +0900

    Feat: 파일 요청시 처리하기 위한 설정 추가

--- src/main/java/myproject/shoppingmall/global/WebConfig.java
+++ src/main/java/myproject/shoppingmall/global/WebConfig.java
@@ -1,24 +1,35 @@
 package myproject.shoppingmall.global;
 
 import myproject.shoppingmall.global.interceptor.LoginCheckInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload.path}")
+    private String uploadPath;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new LogInterceptor())
 //                .order(1)
 //                .addPathPatterns("/**")
 //                .excludePathPatterns("/css/**", "/*.ico", "/error");
+//
+//        registry.addInterceptor(new LoginCheckInterceptor())
+//                .order(1)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/","/member/new","/login","/logout",
+//                        "/css/**","/js/**","/assets/**","/*.ico","/error");
+//    }
 
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/","/member/new","/login","/logout",
-                        "/css/**","/js/**","/assets/**","/*.ico","/error");
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:///" + uploadPath);
     }
 }
