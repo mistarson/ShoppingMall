d3ed23d54235c65b4c16cd4408d37d4eafe8ddce
mistarson <yeowuli2@naver.com>
2022-07-02 19:09:04 +0900

    Feat: DB에서 해당 Entity 조회 실패 시, 예외 추가

--- src/main/java/myproject/shoppingmall/global/error/exception/ErrorCode.java
+++ src/main/java/myproject/shoppingmall/global/error/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
 
     // 상품
     REST_STOCK_NOT_EXISTS(400, "상품의 재고가 부족 합니다."),
+    NON_EXISTENT_ITEM(400, "존재하지 않는 아이템입니다."),
     ALREADY_REGISTERED_ITEM(400, "이미 존재하는 아이템 입니다."),
     FIRST_ITEM_IMAGE_NOT_EXISTS(400, "첫번째 상품 이미지는 필수 입력 값 입니다.");
 
